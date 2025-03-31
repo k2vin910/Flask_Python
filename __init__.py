890e8374d24c1827119322a83ba542d1a7e61c61
k2vin910 <k2vin06@gmail.com>
2025-03-31 16:45:50 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,20 @@
 from flask import Flask
+from flask import render_template
+from flask import json                                                                                                                                     
+app = Flask(__name__)                                                                                                                  
 
-app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
-    etoiles = ''
-    for j in range(1, valeur + 1):
-        etoiles += ' ' * (valeur - j)  # Adding spaces for alignment
-        etoiles += '*' * j  # Adding stars
-        etoiles += '<br>'  # HTML line break
+    etoiles=''
+ for j in range(valeur+ 1):  
+        for i in range(valeur- j):  
+            etoiles += '+'
+        for k in range(j):
+            etoiles += '*'
+        etoiles += '<br>'
     return etoiles
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
