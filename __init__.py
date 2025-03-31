426b0f4cf3b6f2fa0ebfe80c031a57e85216ab8e
k2vin910 <k2vin06@gmail.com>
2025-03-31 12:03:13 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,14 +8,8 @@ app = Flask(__name__)
 def exercice(valeur):
     etoiles = ''
     for i in range(valeur):
-        etoiles += '*' 
-    return etoiles 
-    
-    def carre_etoile(count):
-        count=5
-    for i in range (count):
-        
-    return ('*'* valeur)
+        etoiles += '*' +'<br>'* valeur
+    return etoiles +'<br>'* valeur
 
 
 if __name__ == "__main__":
