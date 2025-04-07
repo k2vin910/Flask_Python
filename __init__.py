f8d13cafe1468bb29ffbce83fc78b7ff94290d4d
k2vin910 <k2vin06@gmail.com>
2025-04-07 11:34:58 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,16 +5,16 @@ app = Flask(__name__)
 
 @app.route('/<int:n>')
 def counter(n):
-    somme = 0  # Initialisation de la somme
+    somme = 0 
 
     for i in range(1, n + 1):
         if i % 11 == 0:
-            continue  # Si divisible par 11, on passe au prochain nombre
+            continue  
         if i % 5 == 0 or i % 7 == 0:
-            somme += i  # Si divisible par 5 ou 7, on ajoute à la somme
-
-        if somme >= 5000:
-            break  # Si la somme dépasse 5000, on arrête la boucle
+        if somme +i > 5000:
+            break  
+            somme +=i
+            
 
     return f"La somme finale est : {somme}"
 
