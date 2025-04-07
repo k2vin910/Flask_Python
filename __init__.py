613a13fafde4a8b262b84431c6655a83cd1f7ff5
k2vin910 <k2vin06@gmail.com>
2025-04-07 11:20:46 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,13 +4,19 @@ from flask import json
 app = Flask(__name__)
 
 @app.route('/<int:valeur>')
-def exercice(n):
-    count = ['0', '1'] if n > 1 else ['0']
+def counter(n):
+    somme = 0  # Initialisation de la somme
 
-    for _ in range(2, n):
-        count.append(str(int(count[-1]) + int(count[-2])))
+    for i in range(1, n + 1):
+        if i % 11 == 0:
+            continue  # Si divisible par 11, on passe au prochain nombre
+        if i % 5 == 0 or i % 7 == 0:
+            somme += i  # Si divisible par 5 ou 7, on ajoute à la somme
 
-    return ', '.join(count[:n])
+        if somme => 5000:
+            break  # Si la somme dépasse 5000, on arrête la boucle
+
+    return f"La somme finale est : {somme}"
 
 if __name__ == "__main__":
     app.run(debug=True)
