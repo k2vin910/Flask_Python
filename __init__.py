5314224304fc699c6865a38bd61426187965c0ba
k2vin910 <k2vin06@gmail.com>
2025-04-07 10:02:28 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,21 +1,23 @@
 from flask import Flask
 from flask import render_template
 from flask import json                                                                                                                                     
-app = Flask(__name__)                                                                                                                  
+app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
-    a,b=0,1
-count= str[str(a)]
+    a, b = 0, 1
+    count = [str(0)] 
+
     if valeur > 1:
-        count.append(str(b))  
+        count.append(str(b))
+
     for _ in range(2, valeur):
         c = a + b
         count.append(str(c))
         a, b = b, c
-return ', '.join(count)
 
-
+    return ', '.join(count)  
 
 if __name__ == "__main__":
     app.run(debug=True)
+
